312c7420658b86987e2cbf2f28d4d837adeac3a2
Laëtitia Alsina <laetitiaalsina@air-de-laetitia.lan>
2023-11-01 10:58:08 +0100

    finished commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Section from './components/Section'
 function App() {
   return (
     <div className="site">
-      <img src="https://lereacteur-react-netflix.netlify.app/static/media/logo.696c2101.png" alt="logo" />
+      <img className='logo' src="https://lereacteur-react-netflix.netlify.app/static/media/logo.696c2101.png" alt="logo" />
     {Movies.map((elem)=>{
       console.log(elem);
+      // console.log(elem.images);
       return(
-        <Section key={elem.id} category={elem.category} image={elem.image}/>
+        <Section key={elem.id} category={elem.category} image={elem.images}/>
       )
     })}
     </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,13 +1,14 @@
 const Section = ({category, image})=>{
-    console.log(category, image);
+    // console.log(image);
 return (
     <div className="section"> 
     <p className="name">{category}</p> 
-    
+
     <div className="imgMovies">
     {image.map((img)=> {
+        // console.log(img);
       return(
-      <img key={img} src='./assets/movies.json' alt="images" />
+      <img key={img} src= {img} alt="images" />
       )})}
       </div>
     </div>
